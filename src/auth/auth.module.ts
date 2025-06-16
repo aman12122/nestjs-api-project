2c48020bf18b4e1b4a38a556767d217c350743c7
Aman <amanvishwanath@gmail.com>
2025-06-16 13:29:25 -0400

    16/6/2025

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-
+import { PrismaModule } from "../prisma/prisma.module"; // Add this import
 
 @Module({
+  imports: [PrismaModule], // Add PrismaModule to imports
   controllers: [AuthController],
   providers: [AuthService],
 })
